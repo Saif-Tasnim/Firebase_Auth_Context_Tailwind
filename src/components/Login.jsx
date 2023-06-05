ecbfc8829783bf3d89ba9939f58e42c4dac83854
Saif-Tasnim <saiftasnim2002@gmail.com>
2023-06-05 08:37:20 +0600

    basc project setup uploded

---
+++ src/components/Login.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Login = () => {
+    return (
+        <div>
+            <h1>Please Log In First !!! </h1>
+        </div>
+    );
+};
+
+export default Login;
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDsg-GsOaesQUWreIiGjzBhB7G2lvis-Mc",
+  authDomain: "fir-auth-context-tailwin-a919a.firebaseapp.com",
+  projectId: "fir-auth-context-tailwin-a919a",
+  storageBucket: "fir-auth-context-tailwin-a919a.appspot.com",
+  messagingSenderId: "117460496566",
+  appId: "1:117460496566:web:b9a9e13a8758080b3b7617"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
